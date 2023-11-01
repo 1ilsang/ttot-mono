290238537f3e8d6d26bf64003c25f7cd982e627b
sangchul-lee <sangchul.lee@linecorp.com>
2023-11-01 18:35:23 +0900

    dump

--- src/apps/front/src/App.tsx
+++ src/apps/front/src/App.tsx
@@ -1,16 +1,66 @@
-import { useLayoutEffect } from 'react';
+import {
+  ChangeEventHandler,
+  KeyboardEventHandler,
+  MouseEventHandler,
+  useState,
+} from 'react';
 import './App.css';
 
 const App = () => {
-  useLayoutEffect(() => {
-    const apiCall = () => {
-      fetch('http://127.0.0.1:3000');
-    };
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const [token, setToken] = useState('');
 
-    apiCall();
-  }, []);
+  const handleIdChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setId(e.target.value);
+  };
+  const handlePwChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPw(e.target.value);
+  };
+  const handlePwKeyDown: KeyboardEventHandler<HTMLInputElement> = async (e) => {
+    if (e.key !== 'Enter') return;
+    const body = JSON.stringify({ id, pw });
+    const res = await fetch('http://127.0.0.1:3000/login', {
+      method: 'post',
+      body,
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const { accessToken } = await res.json();
+    setToken(accessToken);
+  };
+  const handleAuthClick: MouseEventHandler<HTMLButtonElement> = async () => {
+    await fetch('http://127.0.0.1:3000/verify', {
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `bearer ${token}`,
+      },
+    });
+  };
 
-  return <div>Hi</div>;
+  return (
+    <div>
+      <div>
+        <label htmlFor="id">아이디</label>
+        <input id="id" onChange={handleIdChange} value={id} />
+      </div>
+      <div>
+        <label htmlFor="pw">비밀번호</label>
+        <input
+          id="pw"
+          onChange={handlePwChange}
+          value={pw}
+          onKeyDown={handlePwKeyDown}
+        />
+      </div>
+      <div>
+        <button onClick={handleAuthClick}>인증 확인</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
